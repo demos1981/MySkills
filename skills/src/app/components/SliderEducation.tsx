4ba5e1d2554d4483e0b42e47df903f72cfdb24fc
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-09-23 22:04:46 +0300

    education:add style tailwind

--- skills/src/app/components/SliderEducation.tsx
+++ skills/src/app/components/SliderEducation.tsx
@@ -3,16 +3,16 @@ import React from "react";
 const SliderEducation: React.FC = () => {
   return (
     <div className="flex flex-row justify-center items-center  p-6 ">
-      <div className=" relative slideItem block-item bg-blue-500 w-32 h-64 text-white text-center flex items-center justify-center ">
+      <div className=" slideItem relative  block-item bg-blue-500 w-32 h-64 text-white text-center flex items-center justify-center ">
         Block 1
       </div>
-      <div className=" relative slideItem block-item bg-green-500 w-32 h-64 text-white text-center flex items-center justify-center  mx-1 ">
+      <div className=" slideItem relative  block-item bg-green-500 w-32 h-64 text-white text-center flex items-center justify-center  mx-1 ">
         Block 2
       </div>
-      <div className=" relative slideItem block-item bg-red-500 w-32 h-64 text-white text-center flex items-center justify-center  mx-1 ">
+      <div className=" slideItem relative  block-item bg-red-500 w-32 h-64 text-white text-center flex items-center justify-center  mx-1 ">
         Block 3
       </div>
-      <div className=" relative slideItem block-item bg-yellow-500 w-32 h-64 text-white text-center flex items-center justify-center mx-1">
+      <div className=" slideItem relative  block-item bg-yellow-500 w-32 h-64 text-white text-center flex items-center justify-center mx-1">
         Block 4
       </div>
     </div>

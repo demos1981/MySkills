2927a175bfc93c04de78dcf41cd6bb4dea22ebc8
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-09-24 02:50:04 +0300

    education: change styles

--- skills/src/app/components/SliderEducation.tsx
+++ skills/src/app/components/SliderEducation.tsx
@@ -4,9 +4,7 @@ import styles from "./SliderEducation.module.css";
 const SliderEducation: React.FC = () => {
   return (
     <div className="flex flex-row justify-center items-center  p-6 ">
-      <div className={styles.slideItem}>
-        <span className={styles.nameItem}> Block 1</span>
-      </div>
+      <div className={styles.slideItem}>Block 1</div>
       <div className={styles.slideItem}>Block 2</div>
       <div className={styles.slideItem}>Block 3</div>
       <div className={styles.slideItem}>Block 4</div>

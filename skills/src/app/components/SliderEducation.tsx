c9d145fa171294bd913b307e3e00b6b5c9d9b8e9
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-10-11 00:00:47 +0300

    education: add map function

--- skills/src/app/components/SliderEducation.tsx
+++ skills/src/app/components/SliderEducation.tsx
@@ -1,30 +1,41 @@
 import React from "react";
+
 import styles from "./SliderEducation.module.css";
+import { slides } from "./data/slides";
 
 const SliderEducation: React.FC = () => {
   return (
     <div className={styles.slide}>
-      <div className="flex flex-col mr-4">
-        <div className={styles.slide_button}>FrontEnd</div>
-        <div className={styles.slide_content}>FrontEnd</div>
-        <div className={styles.slide_item}></div>
-      </div>
-      <div className="flex flex-col mr-4">
-        <div className={styles.slide_button}>FrontEnd Pro</div>
-        <div className={styles.slide_content}>FrontEnd Pro</div>
-        <div className={styles.slide_item}></div>
-      </div>
-      <div className="flex flex-col mr-4">
-        <div className={styles.slide_button}>TypeScript</div>
-        <div className={styles.slide_content}>TypeScript</div>
-        <div className={styles.slide_item}></div>
-      </div>
-      <div className="flex flex-col mr-4">
-        <div className={styles.slide_button}>Node</div>
-        <div className={styles.slide_content}>Node</div>
-        <div className={styles.slide_item}></div>
-      </div>
+      {slides.map((slide) => (
+        <div key={slide.id} className="flex flex-col mr-4">
+          <div className={styles.slide_button}>{slide.title}</div>
+          <div className={styles.slide_content}>{slide.content}</div>
+          <div className={styles.slide_item}></div>
+        </div>
+      ))}
     </div>
+    // <div className={styles.slide}>
+    //   <div className="flex flex-col mr-4">
+    //     <div className={styles.slide_button}>FrontEnd</div>
+    //     <div className={styles.slide_content}>FrontEnd</div>
+    //     <div className={styles.slide_item}></div>
+    //   </div>
+    //   <div className="flex flex-col mr-4">
+    //     <div className={styles.slide_button}>FrontEnd Pro</div>
+    //     <div className={styles.slide_content}>FrontEnd Pro</div>
+    //     <div className={styles.slide_item}></div>
+    //   </div>
+    //   <div className="flex flex-col mr-4">
+    //     <div className={styles.slide_button}>TypeScript</div>
+    //     <div className={styles.slide_content}>TypeScript</div>
+    //     <div className={styles.slide_item}></div>
+    //   </div>
+    //   <div className="flex flex-col mr-4">
+    //     <div className={styles.slide_button}>Node</div>
+    //     <div className={styles.slide_content}>Node</div>
+    //     <div className={styles.slide_item}></div>
+    //   </div>
+    // </div>
   );
 };
 

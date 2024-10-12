c3f260ac34d148556026f9710d4d9c0bff397d73
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-10-13 00:54:00 +0300

    education: change style components

--- skills/src/app/education/page.tsx
+++ skills/src/app/education/page.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { Metadata } from "next";
 import IconsRound from "../components/IconsRound";
-
+import styles from "./educationPage.module.css";
 import SliderEducation from "../components/SliderEducation";
 
-import { slugs } from "../components/data/dataEducation";
-import BlurText from "../components/BlurText";
+import { slugs } from "../../data/dataEducation";
+
+// import PictureEducation from "../components/PictureEducation";
 export const metadata: Metadata = {
   title: "Education",
   description: "All my education ",
@@ -16,21 +17,19 @@ const Education: FC = () => {
   return (
     <>
       <h1 className="text-4xl font-bold pl-6 pt-6">Education</h1>
-      <main className="container flex box-border">
+      <main className={styles.educationPage}>
         <section className="container mx-auto p-8">
           <div className="relative flex h-full w-full max-w-[32rem] items-center justify-center overflow-hidden rounded-lg  bg-background px-20 pb-20 pt-8 ">
             <IconsRound iconSlugs={slugs} />
           </div>
         </section>
+        {/* <section className="container flex justify-center mx-auto p-10">
+          <PictureEducation />
+        </section> */}
         <section className="container flex justify-end flex-wrap">
           <SliderEducation />
         </section>
       </main>
-      <BlurText
-        text="Its all about my education, come and reading. "
-        className="custom-class text-base-baseHeaderColor ml-7"
-        delay={300}
-      />
     </>
   );
 };
---
+++ skills/src/data/slides.ts
@@ -0,0 +1,26 @@
+// import frontStart from "../assets/front_start.svg";
+// import frontPro from "../assets/frontend.svg";
+
+export const slides = [
+  {
+    id: 2,
+    title: "Front End Pro",
+    content: "Hillel ITSchool",
+    text: "HTML, CSS, JAVASCRIPT, REACT, REDUX-TOOLKIT and more. ",
+    url: "https://certificate.ithillel.ua/view/36319337",
+  },
+  {
+    id: 3,
+    title: "Typescript",
+    content: "Hillel ITSchool",
+    text: "Data typing in class and functional components. Design patterns.",
+    url: "https://certificate.ithillel.ua/view/69011788",
+  },
+  {
+    id: 4,
+    title: "Node.js",
+    content: "Hillel ITSchool",
+    text: "Server Creation. Working with databases. REST API. EXPRESS.JS",
+    url: "https://certificate.ithillel.ua/view/57146233",
+  },
+];

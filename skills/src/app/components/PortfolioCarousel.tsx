0665bf9a53eb6fe4cd8efe782200c90b92cf00da
Dmitriy/ <50639080+demos1981@users.noreply.github.com>
2024-10-14 21:36:28 +0300

    portfolio: add components

---
+++ skills/src/app/components/PortfolioCarousel.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import React from "react";
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import ClassNames from "embla-carousel-class-names";
+import "../components/PortfolioCarousel.module.css";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "./PortfolioCarouselArrowButton";
+import { DotButton, useDotButton } from "./PortfolioCarouselDotButton";
+import Image from "next/image";
+
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType;
+};
+
+const PortfolioCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [ClassNames()]);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide embla__class-names" key={index}>
+              <Image
+                className="embla__slide__img"
+                width={300}
+                height={150}
+                src={`https://picsum.photos/600/350?v=${index}`}
+                alt="Your alt text"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioCarousel;
--- skills/src/app/portfolio/page.tsx
+++ skills/src/app/portfolio/page.tsx
@@ -1,17 +1,22 @@
 import { FC } from "react";
+
 import { Metadata } from "next";
-import Slider from "../components/Slider";
+import { EmblaOptionsType } from "embla-carousel";
+import PortfolioCarousel from "../components/PortfolioCarousel";
 
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "All my work ",
   keywords: "web developer portfolio",
 };
+const OPTIONS: EmblaOptionsType = {};
+const SLIDE_COUNT = 5;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 const Portfolio: FC = () => {
   return (
     <>
-      <Slider />
+      <PortfolioCarousel slides={SLIDES} options={OPTIONS} />
     </>
   );
 };
